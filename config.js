b8b9ac3ffd33a3f655fd69cceb491941cc40ce99
leminhhienhihihi <cugon4704@gmail.com>
2024-09-11 22:11:53 +0700

    Update config.js
    thaydoi

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Mầy Thích Em Hiển À',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
